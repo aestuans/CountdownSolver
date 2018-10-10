67babc03b97aa45c80ea4eaf4ed47e7fc021d090
Ehsan <vyatana@gmail.com>
2018-10-10 22:31:50 +0200

    Refactor and optimization overhaul

--- countdown/countdown.cpp
+++ countdown/countdown.cpp
@@ -8,11 +8,62 @@
 #include <cmath>
 #include <tuple>
 #include <numeric>
+#include <bitset>
 
-int POSSIBLE_OPERATORS_NUM = 5;
+int POSSIBLE_OPERATORS_NUM = 7;
+
+using nums_with_expressions = std::vector< std::pair<int, std::vector<int> > >;
+
 
 std::string binary_operation_presentation(const std::string &lhs, const std::string &op, const std::string &rhs) {
     return lhs + " " + op + " " + rhs;
+}
+
+std::pair<int, std::string> operate(int lhs, int rhs, int op) {
+    /*
+     * Evaluates and returns the value and string representation of a
+     * binary operation.
+     */
+    int res = 0;
+    std::string presentation;
+    switch(op){
+        case -1:
+            res = lhs + rhs;
+            presentation = binary_operation_presentation(std::to_string(lhs), "+", std::to_string(rhs));
+            return std::make_pair(res, presentation);
+        case -2:
+            if(rhs > lhs)
+                return std::make_pair(-1, "");
+            res = lhs - rhs;
+            presentation = binary_operation_presentation(std::to_string(lhs), "-", std::to_string(rhs));
+            return std::make_pair(res, presentation);
+        case -3:
+            res = lhs * rhs;
+            presentation = binary_operation_presentation(std::to_string(lhs), "*", std::to_string(rhs));
+            return std::make_pair(res, presentation);
+        case -4:
+            if(rhs == 0 or lhs % rhs)
+                return std::make_pair(-1, "");
+            res = lhs / rhs;
+            presentation = binary_operation_presentation(std::to_string(lhs), "/", std::to_string(rhs));
+            return std::make_pair(res, presentation);
+        case -5:
+            if(rhs < lhs)
+                return std::make_pair(-1, "");
+            res = rhs - lhs;
+            presentation = binary_operation_presentation(std::to_string(rhs), "-", std::to_string(lhs));
+            return std::make_pair(res, presentation);
+        case -6:
+            if(lhs == 0 or rhs % lhs)
+                return std::make_pair(-1, "");
+            res = rhs / lhs;
+            presentation = binary_operation_presentation(std::to_string(rhs), "/", std::to_string(lhs));
+            return std::make_pair(res, presentation);
+        case -7:
+            return std::make_pair(lhs, std::to_string(lhs));
+        default:
+            break;
+    }
 }
 
 std::pair<int, std::string> evaluate_postfix(std::vector<int> expression)
@@ -41,35 +92,10 @@ std::pair<int, std::string> evaluate_postfix(std::vector<int> expression)
             int lhs = evaluation_stack.top();
             evaluation_stack.pop();
 
-            int res = -1;
+            std::pair<int, std::string> temp = operate(lhs, rhs, i);
+            int res = temp.first;
+            presentation += temp.second;
 
-            switch(i){
-                case -1:
-                    res = lhs + rhs;
-                    presentation += binary_operation_presentation(std::to_string(lhs), "+", std::to_string(rhs));
-                    break;
-                case -2:
-                    if(rhs > lhs)
-                        return std::make_pair(-1, "");
-                    res = lhs - rhs;
-                    presentation += binary_operation_presentation(std::to_string(lhs), "-", std::to_string(rhs));
-                    break;
-                case -3:
-                    res = lhs * rhs;
-                    presentation += binary_operation_presentation(std::to_string(lhs), "*", std::to_string(rhs));
-                    break;
-                case -4:
-                    if(rhs == 0 or lhs % rhs)
-                        return std::make_pair(-1, "");
-                    res = lhs / rhs;
-                    presentation += binary_operation_presentation(std::to_string(lhs), "/", std::to_string(rhs));
-                    break;
-                case -5:
-                    res = lhs;
-                    break;
-                default:
-                    break;
-            }
             evaluation_stack.push(res);
             presentation += " = " + std::to_string(res) + "\n";
         }
@@ -77,77 +103,54 @@ std::pair<int, std::string> evaluate_postfix(std::vector<int> expression)
     return make_pair(evaluation_stack.top(), presentation);
 }
 
-int make_operations(int n, int target, std::vector<int> ops,
-                    std::vector<int> ops_distribution, std::vector<int> nums) {
+int solve_countdown(int target, nums_with_expressions numbers,
+                    std::vector<bool> used, std::vector<int> &solution) {
     /*
-     * Recursively makes all possible operations to fill ops_distribution, constructs
-     * the expression and compares it with target.
-     * Returns 0 if expression evaluated in target is found.
-     * Returns 1 otherwise.
+     * Recursively creates and evaluates all possible expressions from numbers
+     * and stores any possible solution to solution.
+     * Returns 0 if solution found, 1 otherwise.
      */
-    if(ops.size() < n) {
-        for(int i = -POSSIBLE_OPERATORS_NUM; i < 0; i++) {
-            ops.push_back(i);
-            if(!make_operations(n, target, ops, ops_distribution, nums))
-                return 0;
-            ops.pop_back();
-        }
-    }
-    else {
-        std::vector<int> temp;
-        temp.push_back(nums[0]);
-        temp.push_back(nums[1]);
+    nums_with_expressions _numbers = numbers;
+    for(int i = 0; i < numbers.size(); i++) {
+        for(int j = i + 1; j < numbers.size(); j++){
+            if(used[i] || used[j])
+                continue;
+            used[j] = true;
 
-        int k = 0;
-        for(int i = 0; i < 5; i++){
-            for(int l = 0; l < ops_distribution[i]; l++){
-                temp.push_back(ops[k]);
-                k++;
+            //Shorter names for convenience
+            int& numi = numbers[i].first;
+            std::vector<int>& numi_expression = numbers[i].second;
+            int& numj = numbers[j].first;
+            std::vector<int>& numj_expression = numbers[j].second;
+
+            for(int op = -POSSIBLE_OPERATORS_NUM; op < 0; op++) {
+
+                //Result is saved to the lhs operand's place
+                //rhs operand is set to used.
+                numi = operate(numi, numj, op).first;
+
+                //append numj_expr to numi_expr and add operator
+                numi_expression.insert(numi_expression.end(), numj_expression.begin(), numj_expression.end());
+                numi_expression.push_back(op);
+
+                if(numi < 0)
+                    continue;
+
+                if(numi == target) {
+                    solution = numi_expression;
+                    return 0;
+                }
+
+                if(!solve_countdown(target, numbers, used, solution))
+                    return 0;
+
+                numi = _numbers[i].first;
+                numi_expression = _numbers[i].second;
             }
-            if(i < 4)
-                temp.push_back(nums[i + 2]);
-        }
+            used[j] = false;
 
-        int res = evaluate_postfix(temp).first;
-        if(res == target){
-            for (int i : temp)
-                std::cout << i << " ";
-            std::cout << std::endl;
-            std::cout << evaluate_postfix(temp).second;
-            return 0;
         }
-
     }
     return 1;
-}
 
-int make_expressions(int target, std::vector<int> ops_distribution, std::vector<int> &numbers) {
-    /*
-     * Recursively makes all possible operators distribution and calls make_operations
-     * To fill the distribution and evaluate.
-     * Returns 0 if target found, 1 otherwise.
-     */
-    auto ops_num = static_cast<int>(numbers.size() - 1);
-    auto ops_size = static_cast<int>(ops_distribution.size());
-    if(ops_size < ops_num - 1) {
-        int sum = accumulate(ops_distribution.begin(), ops_distribution.end(), 0);
-
-        // Number of spots left for operators
-        int remaining_spots = (ops_size == 0) ? 2 : std::min(ops_size, ops_size - sum) + 2;
-
-        for(int x = 0; x < remaining_spots; x++) {
-            ops_distribution.push_back(x);
-            if(!make_expressions(target, ops_distribution, numbers))
-                return 0;
-            ops_distribution.pop_back();
-        }
-    }
-    else {
-        int sum = accumulate(ops_distribution.begin(), ops_distribution.end(), 0);
-        ops_distribution.push_back(ops_num - sum);
-
-        if(!make_operations(ops_num, target, std::vector<int>(), ops_distribution, numbers))
-            return 0;
-    }
-    return 1;
 }
